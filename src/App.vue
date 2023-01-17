edec9f403bf699cd10ce6210c302d69539d7199f
Virginie <vb@ikimo9.com>
2023-01-17 11:10:27 +0100

    ajout starter files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
 
   <div class="container">
     <router-view></router-view>
