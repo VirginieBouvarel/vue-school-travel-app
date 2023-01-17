71020789ca26851b96aa21d779d17faaadd5bb13
Virginie <vb@ikimo9.com>
2023-01-17 10:17:15 +0100

    ajout vue-router + routes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -5,4 +7,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
