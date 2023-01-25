da898f13e2b6e76345471b4ca4a6e1f7619a526a
Virginie <vb@ikimo9.com>
2023-01-25 08:37:16 +0100

    ajout route imbriquée pour les expériences

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <img :src="`/images/${experience.image}`" :alt="experience.name" width="250" height="150"/>
+    <span class="card-text">{{  experience.name  }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      experience: { type: Object, required: true }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
