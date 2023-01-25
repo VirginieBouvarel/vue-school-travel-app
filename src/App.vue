47f37d08f836f4e6d07ff212e4077ba2603713bf
Virginie <vb@ikimo9.com>
2023-01-25 09:04:04 +0100

    ajout transition entre les vues

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,25 @@
   <TheNavigation />
 
   <div class="container">
-    <router-view></router-view>
+    <router-view v-slot="{Component}">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup>
   import TheNavigation from '@/components/TheNavigation.vue';
 </script>
+
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
