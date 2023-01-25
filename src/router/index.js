87e546356e6bf12f8f7e8c3e663544bd4eefb37c
Virginie <vb@ikimo9.com>
2023-01-25 10:36:05 +0100

    ajout NotFound +  beforeEnter gard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   { path: '/', name: 'Home', component: Home},
@@ -8,6 +9,15 @@ const routes = [
     name: 'destination.show', 
     component: () => import('@/views/DestinationShow.vue'),
     props: route => ({ ...route.params, id : parseInt(route.params.id)}),
+    beforeEnter(to, from) {
+      const exists = sourceData.destinations.find(destination => destination.id === parseInt(to.params.id));
+      if (!exists) return { 
+        name: 'NotFound',
+        params: { pathMatch: to.path.split('/').slice(1) },
+        query: to.query,
+        hash: to.hash,
+      };
+    },
     children: [
       { 
         path: ':experienceSlug', 
@@ -17,11 +27,21 @@ const routes = [
       },
     ]
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: ()=> import('@/views/NotFound.vue')
+  }
 ]
 
 const router = createRouter({
   history : createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || new Promise((resolve) => {
+      setTimeout(() => resolve({ top: 0, behavior: 'smooth'}), 300);
+    });
+  }
 })
 
 export default router
