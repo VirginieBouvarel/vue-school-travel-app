f1caa8a38cc27258856fce3e59d3c83b80057306
Virginie <vb@ikimo9.com>
2023-01-31 10:05:16 +0100

    ajout AppLink

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="nav">
-    <router-link id="logo" to="/">Vue School Travel App</router-link>
+  <AppLink id="logo" to="/">Vue School Travel App</AppLink>
 
-    <router-link
+  <AppLink
       v-for="destination in destinations"
       :key="destination.id"
       :to="{ name: 'destination.show', params: { id: destination.id, slug: destination.slug }}"
-    >
+  >
       {{ destination.name }}
-    </router-link>
+  </AppLink>
+  <AppLink :to="{name: 'protected'}">Dashboard</AppLink>
+  <AppLink to="https://vueschool.io">Vue School</AppLink>
   </div>
 </template>
 
@@ -16,10 +18,10 @@
 import sourceData from '@/data.json';
 
 export default {
-  data() {
-    return {
-      destinations: sourceData.destinations,
-    }
-  }
+    data() {
+        return {
+            destinations: sourceData.destinations,
+        };
+    },
 }
 </script>
